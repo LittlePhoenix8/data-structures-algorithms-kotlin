b46aa5136c289696eecf24a906a27b6acfa4b7cc
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-03 11:36:28 -0300

    single number

---
+++ src/lettcodeexercices/SingleNumber.kt
@@ -0,0 +1,33 @@
+package lettcodeexercices
+
+fun main() {
+ val singleNumber = SingleNumber()
+    println(singleNumber.singleNumber(intArrayOf(2,2,1))) //1
+    println(singleNumber.singleNumber(intArrayOf(4,1,2,1,2))) //4
+    println(singleNumber.singleNumber(intArrayOf(1))) //1
+}
+
+class SingleNumber {
+    fun singleNumber(nums: IntArray): Int {
+        if(nums.size == 1) {
+            return nums.first()
+        }
+        val hashmap = HashMap<Int, Int>()
+
+        for(num in nums) {
+            if(hashmap.containsKey(num)) {
+                hashmap[num] = hashmap[num]!! + 1
+            } else {
+                hashmap[num] = 1
+            }
+        }
+
+        for(num in nums) {
+            if(hashmap.containsKey(num) && hashmap[num]!! == 1) {
+                return num
+            }
+        }
+
+        return 0
+    }
+}
