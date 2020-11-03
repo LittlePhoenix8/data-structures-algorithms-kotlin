e6c10a5f05d4b74447568c5590fe9b1d3b50ca86
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-03 15:23:17 -0300

    number of zeroes

---
+++ src/lettcodeexercices/NumberOfZeroes.kt
@@ -0,0 +1,42 @@
+package lettcodeexercices
+
+fun main() {
+    val numberOfZeroes = NumberOfZeroes()
+    val array = intArrayOf(0,1,0,3,12)
+    numberOfZeroes.moveZeroes(array)
+    println(array.joinToString(""))
+}
+
+class NumberOfZeroes {
+    fun moveZeroes(nums: IntArray) {
+        if (nums.isNotEmpty()) {
+            var restOfZeroes = 0
+            for (i in nums.indices) {
+                if (nums[i] == 0) {
+                    restOfZeroes++
+                } else if (restOfZeroes > 0) {
+                    nums[i - restOfZeroes] = nums[i]
+                    nums[i] = 0
+                }
+            }
+        }
+    }
+
+    fun moveZeroes2(nums: IntArray) {
+        var numberOfZeros = 0
+        var index = 0
+        for(i in nums.indices) {
+            if(nums[i] == 0) {
+                numberOfZeros++
+            } else {
+                nums[index] = nums[i]
+                index++
+            }
+        }
+        if(numberOfZeros > 0) {
+            for(i in nums.size - 1 downTo nums.size - numberOfZeros) {
+                nums[i] = 0
+            }
+        }
+    }
+}
