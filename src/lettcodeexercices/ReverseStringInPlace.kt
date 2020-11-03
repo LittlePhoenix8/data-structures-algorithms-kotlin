777e2b9345778dfbd89f51df3316b9bd8eaf2007
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-03 10:47:57 -0300

    Reverse string in place

---
+++ src/lettcodeexercices/ReverseStringInPlace.kt
@@ -0,0 +1,22 @@
+package lettcodeexercices
+
+fun main() {
+    val reverseStringInPlace = ReverseStringInPlace()
+    reverseStringInPlace.reverseString(charArrayOf('h','e','l','l','o'))
+}
+
+class ReverseStringInPlace {
+    fun reverseString(s: CharArray) {
+        println(s)
+        var i = 0
+        var j = s.size - 1
+        while(i < j) {
+            val temp = s[j]
+            s[j] = s[i]
+            s[i] = temp
+            i++
+            j--
+        }
+        println(s)
+    }
+}
