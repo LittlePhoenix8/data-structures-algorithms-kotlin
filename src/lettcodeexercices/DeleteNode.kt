c7cdfc414bfb195598aa2fac6ef32a2431ada0f9
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-04 14:47:13 -0300

    delete node formt singly linked list

---
+++ src/lettcodeexercices/DeleteNode.kt
@@ -0,0 +1,28 @@
+package lettcodeexercices
+
+fun main() {
+    val node1 = ListNode(2)
+    val node2 = ListNode(0)
+    val node3 = ListNode(1)
+    val node4 = ListNode(3)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = null
+
+    val deleteNode = DeleteNode()
+    deleteNode.deleteNode(node1)
+
+    println(node1.print())
+}
+
+class DeleteNode {
+    fun deleteNode(node: ListNode?) {
+        if (node == null) {
+            return
+        }
+        node.`val` = node.next!!.`val`
+        node.next = node.next!!.next
+    }
+}
