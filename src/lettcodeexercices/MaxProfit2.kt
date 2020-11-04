0a15b80bee139d5b6beb2b724a70d9240bd10ebd
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-04 16:05:53 -0300

    max profit 2

---
+++ src/lettcodeexercices/MaxProfit2.kt
@@ -0,0 +1,23 @@
+package lettcodeexercices
+
+fun main() {
+    val maxProfit2 = MaxProfit2()
+    println(maxProfit2.maxProfit(intArrayOf(7,1,5,3,6,4))) //7
+    println(maxProfit2.maxProfit(intArrayOf(1,2,3,4,5))) //4
+    println(maxProfit2.maxProfit(intArrayOf(7,6,4,3,1))) //0
+}
+
+class MaxProfit2 {
+    fun maxProfit(prices: IntArray): Int {
+        var maxProfit = 0
+        for (i in prices.indices) {
+            if (i + 1 < prices.size) {
+                val profit = prices[i + 1] - prices[i]
+                if (profit > 0) {
+                    maxProfit += profit
+                }
+            }
+        }
+        return maxProfit
+    }
+}
