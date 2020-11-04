8da5808efdcbe8dd52f7775b6477e1b54965fb85
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-04 17:19:13 -0300

    contains duplicate

---
+++ src/lettcodeexercices/ContainsDuplicate.kt
@@ -0,0 +1,22 @@
+package lettcodeexercices
+
+fun main() {
+    val containsDuplicate = ContainsDuplicate()
+    println(containsDuplicate.containsDuplicate(intArrayOf(1,2,3,1))) //true
+    println(containsDuplicate.containsDuplicate(intArrayOf(1,2,3,4))) //false
+    println(containsDuplicate.containsDuplicate(intArrayOf(1,1,1,3,3,4,3,2,4,2))) //true
+}
+
+class ContainsDuplicate {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val hashmap = HashMap<Int,Boolean>()
+        for(i in nums.indices) {
+            if(!hashmap.containsKey(nums[i])) {
+                hashmap[nums[i]] = true
+            } else if(hashmap[nums[i]]!!) {
+                return true
+            }
+        }
+        return false
+    }
+}
