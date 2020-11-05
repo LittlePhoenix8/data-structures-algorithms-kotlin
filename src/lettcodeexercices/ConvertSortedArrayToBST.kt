b32cd054ff40fc53bfbab63553241bfe2e52ac4a
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-05 15:02:49 -0300

    sorted array to bst

---
+++ src/lettcodeexercices/ConvertSortedArrayToBST.kt
@@ -0,0 +1,32 @@
+package lettcodeexercices
+
+import com.google.gson.Gson
+
+fun main() {
+    val convertSortedArrayToBST = ConvertSortedArrayToBST()
+    val tree = convertSortedArrayToBST.sortedArrayToBST(intArrayOf(-10,-3,0,5,9))
+    println(Gson().toJson(tree))
+
+    val tree1 = convertSortedArrayToBST.sortedArrayToBST(intArrayOf(0,1,2,3,4,5))
+    println(Gson().toJson(tree1))
+}
+
+class ConvertSortedArrayToBST {
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        if (nums.isEmpty()) {
+            return null
+        }
+        return bstHelper(nums, 0, nums.size - 1)
+    }
+
+    private fun bstHelper(nums: IntArray, low: Int, high: Int): TreeNode? {
+        if (low > high) {
+            return null
+        }
+        val middle = (low + high) / 2
+        val node = TreeNode(nums[middle])
+        node.left = bstHelper(nums, low, middle - 1)
+        node.right = bstHelper(nums, middle + 1, high)
+        return node
+    }
+}
