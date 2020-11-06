0926bbb6eb3b1919c99c5ed9ae691a909f049f63
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-06 16:42:12 -0300

    happy numbers

---
+++ src/lettcodeexercices/HappyNumber.kt
@@ -0,0 +1,33 @@
+package lettcodeexercices
+
+import java.util.*
+
+fun main() {
+    val happyNumber = HappyNumber()
+    println(happyNumber.isHappy(19)) //true
+    println(happyNumber.isHappy(2)) //false
+    println(happyNumber.isHappy(1111111)) //true
+}
+
+class HappyNumber {
+    fun isHappy(n: Int): Boolean {
+        val hashset = HashSet<Int>()
+        var squareSum: Int
+        var remain: Int
+        var number = n
+        while (hashset.add(number)) {
+            squareSum = 0
+            while (number > 0) {
+                remain = number % 10
+                squareSum += remain * remain
+                number /= 10
+            }
+            if (squareSum == 1) {
+                return true
+            } else {
+                number = squareSum
+            }
+        }
+        return false
+    }
+}
