1fc9007ae4745795d7f9742c0a8731b514dfad46
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-09 16:08:26 -0300

    power of three

---
+++ src/lettcodeexercices/PowerOfThree.kt
@@ -0,0 +1,24 @@
+package lettcodeexercices
+
+fun main() {
+    val powerOfThree = PowerOfThree()
+    println(powerOfThree.isPowerOfThree(27))
+    println(powerOfThree.isPowerOfThree(0))
+    println(powerOfThree.isPowerOfThree(9))
+    println(powerOfThree.isPowerOfThree(45))
+    println(powerOfThree.isPowerOfThree(2))
+}
+
+class PowerOfThree {
+    fun isPowerOfThree(n: Int): Boolean {
+        if(n == 0) {
+            return false
+        }
+        var num = n
+        while(num % 3 == 0) {
+            //is divisible by 3
+            num /= 3
+        }
+        return num == 1
+    }
+}
