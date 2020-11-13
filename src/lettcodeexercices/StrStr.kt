bf09212f8a469a678ff5882f5c8a893ffa7916d8
Viviana Garrido <vivianagarridos@gmail.com>
2020-11-13 16:25:56 -0300

    str

---
+++ src/lettcodeexercices/StrStr.kt
@@ -0,0 +1,35 @@
+package lettcodeexercices
+
+fun main() {
+    val strStr = StrStr()
+    println(strStr.strStr("hello", "ll")) //2
+    println(strStr.strStr("aaaaa", "bba")) //-1
+    println(strStr.strStr("", "")) //0
+    println(strStr.strStr("", "a")) //-1
+    println(strStr.strStr("mississippi", "mississippi")) //-1
+}
+
+class StrStr {
+    fun strStr(haystack: String, needle: String): Int {
+        if(needle.isEmpty()) {
+            return 0
+        }
+        if (haystack.isEmpty()) {
+            return -1
+        }
+        for (i in haystack.indices) {
+            if (i + needle.length > haystack.length) {
+                break
+            }
+            for (j in needle.indices) {
+                if (haystack[i + j] != needle[j]) {
+                    break
+                }
+                if (j == needle.length - 1) {
+                    return i
+                }
+            }
+        }
+        return -1
+    }
+}
