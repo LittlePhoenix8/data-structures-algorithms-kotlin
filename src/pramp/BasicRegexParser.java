a1bc1662ed0e3a530f4b203e220d2ae6e82135e5
Viviana Garrido <vivianagarridos@gmail.com>
2021-03-03 16:44:28 -0800

    update basic regex parser

--- src/pramp/BasicRegexParser.java
+++ src/pramp/BasicRegexParser.java
@@ -4,7 +4,7 @@ package pramp;
  * https://www.pramp.com/challenge/KvZ3aL35Ezc5K9Eq9Llp
  * */
 public class BasicRegexParser {
-    static boolean isMatch(String text, String pattern, int indexText, int indexPattern) {
+    static boolean isMatch(String text, String pattern, int i, int j) {
         if(text == null) {
             return false;
         }
@@ -17,36 +17,36 @@ public class BasicRegexParser {
         if(text.equals(pattern)) {
             return true;
         }
-        if (indexText >= text.length()) {
+        if (i >= text.length()) {
             //text index too long
-            if (indexPattern >= pattern.length()) {
+            if (j >= pattern.length()) {
                 //both are done so true
                 return true;
             } else {
                 //pattern not over
-                if (indexPattern + 1 < pattern.length() && pattern.charAt(indexPattern + 1) == '*') {
+                if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
                     //pattern * skip
-                    return isMatch(text, pattern, indexText, indexPattern + 2);
+                    return isMatch(text, pattern, i, j + 2);
                 } else {
                     //no match
                     return false;
                 }
             }
-        } else if (indexPattern >= pattern.length()) {
+        } else if (j >= pattern.length()) {
             return false;
-        } else if (indexPattern + 1 < pattern.length() && pattern.charAt(indexPattern + 1) == '*') {
+        } else if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
             //check if pattern previous is a match
-            if (pattern.charAt(indexPattern) == '.' || pattern.charAt(indexPattern) == text.charAt(indexText)) {
+            if (pattern.charAt(j) == '.' || pattern.charAt(j) == text.charAt(i)) {
                 //is a match
-                //return isMatch(text, pattern, indexText + 1, indexPattern);
-                return (isMatch(text, pattern, indexText, indexPattern + 2) || isMatch(text, pattern, indexText + 1, indexPattern));
+                //return isMatch(text, pattern, i + 1, j);
+                return (isMatch(text, pattern, i, j + 2) || isMatch(text, pattern, i + 1, j));
             } else {
                 //no match, skip pattern *
-                return isMatch(text, pattern, indexText, indexPattern + 1);
+                return isMatch(text, pattern, i, j + 1);
             }
-        } else if (pattern.charAt(indexPattern) == '.' || pattern.charAt(indexPattern) == text.charAt(indexText)) {
+        } else if (pattern.charAt(j) == '.' || pattern.charAt(j) == text.charAt(i)) {
             //wildcard or match
-            return isMatch(text, pattern, indexText + 1, indexPattern + 1);
+            return isMatch(text, pattern, i + 1, j + 1);
         } else {
             //no match or wildcard or *
             return false;
@@ -57,7 +57,50 @@ public class BasicRegexParser {
         return isMatch(text, pattern, 0, 0);
     }
 
+    static boolean isMatchIterative(String text, String pattern) {
+        if (text == null || pattern == null) {
+            return false;
+        }
+        if (text.isEmpty() && pattern.isEmpty()) {
+            return true;
+        }
+        if (text.equals(pattern)) {
+            return true;
+        }
+        int i = 0;
+        int j = 0;
+        while (i < text.length()) {
+            if (j >= pattern.length()) {
+                return false;
+            }
+            char t = text.charAt(i);
+            char p = pattern.charAt(j);
+            if (t == p) {
+                i++;
+                j++;
+            } else {
+                if (p == '.') {
+                    i++;
+                    j++;
+                } else if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                    j += 2;
+                } else if (p == '*'){
+                    if (j - 1 > 0 && pattern.charAt(j - 1) == t) {
+                        i++;
+                    } else {
+                        j++;
+                        //this is for the case text="abaa", pattern="a.*a*"
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+        System.out.println("Recursive");
         System.out.println("Expected: true, Actual: " + isMatch("", ""));
         System.out.println("Expected: false, Actual:  " + isMatch("aa", "a"));
         System.out.println("Expected: true, Actual:  " + isMatch("bb", "bb"));
@@ -66,5 +109,15 @@ public class BasicRegexParser {
         System.out.println("Expected: true, Actual:  " + isMatch("aba", "a.a"));
         System.out.println("Expected: true, Actual:  " + isMatch("acd", "ab*c."));
         System.out.println("Expected: true, Actual:  " + isMatch("abaa", "a.*a*"));
+
+        System.out.println("Iterative");
+        System.out.println("Expected: true, Actual: " + isMatchIterative("", ""));
+        System.out.println("Expected: false, Actual:  " + isMatchIterative("aa", "a"));
+        System.out.println("Expected: true, Actual:  " + isMatchIterative("bb", "bb"));
+        System.out.println("Expected: true, Actual:  " + isMatchIterative("", "a*"));
+        System.out.println("Expected: false, Actual:  " + isMatchIterative("abbdbb", "ab*d"));
+        System.out.println("Expected: true, Actual:  " + isMatchIterative("aba", "a.a"));
+        System.out.println("Expected: true, Actual:  " + isMatchIterative("acd", "ab*c."));
+        System.out.println("Expected: true, Actual:  " + isMatchIterative("abaa", "a.*a*"));
     }
 }
