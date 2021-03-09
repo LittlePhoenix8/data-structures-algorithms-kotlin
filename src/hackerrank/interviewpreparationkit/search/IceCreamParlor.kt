1c5f9c48ce7c27a618786e26515eecd302e8ae96
Viviana Garrido <vivianagarridos@gmail.com>
2021-03-08 16:43:25 -0800

    ice cream parlor

---
+++ src/hackerrank/interviewpreparationkit/search/IceCreamParlor.kt
@@ -0,0 +1,27 @@
+package hackerrank.interviewpreparationkit.search
+
+fun main() {
+    val iceCreamParlor = IceCreamParlor()
+    iceCreamParlor.whatFlavors(arrayOf(1, 4, 5, 3, 2), 4) //1 4
+    iceCreamParlor.whatFlavors(arrayOf(2, 2, 4, 3), 4) //1 2
+    iceCreamParlor.whatFlavors(arrayOf(2, 1, 3, 5, 6), 5) //1 3
+    iceCreamParlor.whatFlavors(arrayOf(4, 3, 2, 5, 7), 8) //2 4
+    iceCreamParlor.whatFlavors(arrayOf(7, 2, 5, 4, 11), 12) //1 3
+    //iceCreamParlor.whatFlavors(arrayOf(), 0) //
+}
+
+class IceCreamParlor {
+    fun whatFlavors(cost: Array<Int>, money: Int) {
+        val hashmap = HashMap<Int, Int>()
+        for (i in cost.indices) {
+            val price = cost[i]
+            val rest = money - price
+            if (hashmap.containsKey(rest)) {
+                println("${hashmap[rest]} ${i + 1}")
+                break
+            } else {
+                hashmap[cost[i]] = i + 1
+            }
+        }
+    }
+}
