c210699a0ae2234336590d77022ca99d5734f91a
Viviana Garrido <vivianagarridos@gmail.com>
2021-03-19 15:28:44 -0700

    shortest reach in graph java & kotlin

---
+++ src/hackerrank/interviewpreparationkit/graphs/ShortestReachInGraph.kt
@@ -0,0 +1,47 @@
+package hackerrank.interviewpreparationkit.graphs
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun main() {
+    val graph = ShortestReachInGraph.Graph(6)
+    val edges = arrayOf(arrayOf(1, 2), arrayOf(2, 3), arrayOf(3, 4), arrayOf(1, 5))
+    for (pair in edges) {
+        graph.addEdge(pair.first(), pair.last())
+    }
+    val distances = graph.shortestReach(1)
+    println(distances.joinToString(", ")) //[6, 12, 18, 6, -1]
+}
+
+class ShortestReachInGraph {
+    class Graph(val size: Int) {
+        private val adjacent = Array<ArrayList<Int>>(size + 1) { ArrayList() }
+        private val edge = 6
+
+        fun addEdge(first: Int, second: Int) {
+            adjacent[first].add(second)
+            adjacent[second].add(first)
+        }
+
+        fun shortestReach(startId: Int): Array<Int> {
+            val distances = Array(size) { -1 }
+            if (distances.size <= 1) {
+                return distances
+            }
+            //dfs from start add edge sum
+            val queue: Queue<Int> = LinkedList()
+            queue.add(startId)
+            distances[startId] = 0
+            while (queue.isNotEmpty()) {
+                val current = queue.remove()
+                for (adj in adjacent[current]) {
+                    if (distances[adj] == -1) {
+                        distances[adj] = distances[current] + edge
+                        queue.add(adj)
+                    }
+                }
+            }
+            return distances
+        }
+    }
+}
---
+++ src/hackerrank/interviewpreparationkit/graphs/ShortestReachInGraph1.java
@@ -0,0 +1,68 @@
+package hackerrank.interviewpreparationkit.graphs;
+
+import java.util.*;
+
+public class ShortestReachInGraph1 {
+    public static class Graph {
+        private final List<List<Integer>> adjacent;
+        private final int size;
+
+        public Graph(int size) {
+            this.size = size;
+            adjacent = new ArrayList<>();
+            for(int i = 0; i < size + 1; i++) {
+                adjacent.add(new ArrayList<>());
+            }
+        }
+
+        public void addEdge(int first, int second) {
+            adjacent.get(first).add(second);
+            adjacent.get(second).add(first);
+        }
+
+        public int[] shortestReach(int startId) { // 0 indexed
+            int[] distances = new int[size];
+            Arrays.fill(distances, -1);
+            if (distances.length <= 1) {
+                return distances;
+            }
+            //dfs from start add edge sum
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(startId);
+            distances[startId] = 0;
+            while (!queue.isEmpty()) {
+                Integer current = queue.poll();
+                for (int adj : adjacent.get(current)) {
+                    if (distances[adj] == -1) {
+                        queue.add(adj);
+                        distances[adj] = distances[current] + 6;
+                    }
+                }
+            }
+            return distances;
+        }
+    }
+
+    public static void main(String[] args) {
+        ShortestReachInGraph1.Graph graph = new ShortestReachInGraph1.Graph(6);
+        int[][] edges = new int[][] { new int[]{ 1, 2 }, new int[]{ 2, 3 }, new int[]{ 3, 4 }, new int[]{ 1, 5 } };
+        for (int[] pair : edges) {
+            graph.addEdge(pair[0], pair[1]);
+        }
+        int[] distances = graph.shortestReach(1);
+        for (int i : distances) {
+            System.out.print(i + ", "); //[6, 12, 18, 6, -1]
+        }
+        System.out.println("");
+
+        ShortestReachInGraph1.Graph graph2 = new ShortestReachInGraph1.Graph(6);
+        int[][] edges2 = new int[][] { new int[]{ 1, 2 }, new int[]{ 2, 3 }, new int[]{ 3, 4 }, new int[]{ 1, 5 } };
+        for (int[] pair : edges2) {
+            graph2.addEdge(pair[0], pair[1]);
+        }
+        int[] distance2 = graph2.shortestReach(1);
+        for (int i : distance2) {
+            System.out.print(i + ", "); //[6, 12, 18, 6, -1]
+        }
+    }
+}
